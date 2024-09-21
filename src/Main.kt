a488fad3b5c3d2e32c83903655f57862d8980895
Mikael Kundert <93661+mikaelkundert@users.noreply.github.com>
2024-09-21 12:35:04 +0300

    Introduce basic validation for first argument

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,22 @@
+import java.io.File
+import kotlin.system.exitProcess
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        exitWithError("Missing argument <path-to-library-root>")
     }
+
+    val musicLibraryRoot = File(args[0])
+    println("Music library root: '${musicLibraryRoot.absolutePath}'")
+
+    if (!musicLibraryRoot.exists()) {
+        exitWithError("Given path '${musicLibraryRoot.path}' does not exist!")
+    }
+}
+
+fun exitWithError(message: String) {
+    System.err.println(message)
+    exitProcess(1)
 }
